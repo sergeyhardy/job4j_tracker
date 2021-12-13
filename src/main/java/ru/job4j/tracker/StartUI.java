4a96287263dc441f574fdf513b3a8134f889d1f9
sergeyhardy <sergeyhardy@gmail.com>
2021-12-13 23:35:00 +0900

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,9 +4,8 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        Item item = new Item(1, "first");
+        System.out.println(item.toString());
 
     }
 
