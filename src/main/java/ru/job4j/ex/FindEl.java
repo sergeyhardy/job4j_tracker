ef219aa482c14695e93b2f6258f9ba7760f702f0
sergeyhardy <sergeyhardy@gmail.com>
2022-01-12 22:20:58 +0900

    0.4. Пользовательские исключения. [#219367 #248255]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,13 +6,14 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
             throw new ElementNotFoundException("Element not found");
-        } else {
-            return rsl;
         }
+        return rsl;
+
     }
 
     public static void main(String[] args) {
