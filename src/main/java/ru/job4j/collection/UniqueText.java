939480d1edcab93dba4f09edd565b33f421475cb
sergeyhardy <sergeyhardy@gmail.com>
2022-04-10 22:19:18 +0900

    2. Идентичные тексты [#212655 #285779]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -8,13 +8,10 @@ public class UniqueText {
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
-        /* for-each origin -> new HashSet. */
         for (String o:origin) {
             check.add(o);
         }
-        /* for-each text -> hashSet.contains */
         for (String t:text) {
-            rsl = check.contains(t);
             if (!check.contains(t)) {
                 rsl = check.contains(t);
                 break;
