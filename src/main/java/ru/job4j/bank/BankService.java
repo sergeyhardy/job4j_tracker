28f6363f431af12b0a04ad0a11b2e6a3ea39634b
sergeyhardy <sergeyhardy@gmail.com>
2022-04-28 23:23:15 +0900

    3. Банковские переводы [#10038 #294377]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -11,21 +11,20 @@ public class BankService {
 
     public void addAccount(String passport, Account account) {
         User requestedUser = findByPassport(passport);
-        List<Account> usersAccounts = new ArrayList<>(users.get(requestedUser));
-        if (!usersAccounts.contains(account)) {
-               usersAccounts.add(account);
+        if (requestedUser != null) {
+            if (!users.get(requestedUser).contains(account)) {
+                users.get(requestedUser).add(account);
             }
-        users.put(requestedUser, usersAccounts);
+        }
     }
 
     public User findByPassport(String passport) {
         User rsl = null;
-        Set<User> keys = users.keySet();
-        for (User user:keys) {
+        for (User user:users.keySet()) {
             if (user.getPassport().equals(passport)) {
             rsl = user;
+            break;
             }
-
         }
         return rsl;
     }
@@ -38,6 +37,7 @@ public class BankService {
             for (Account account :usersAccounts) {
                 if (account.getRequisite().equals(requisite)) {
                     rsl = account;
+                    break;
                 }
             }
         }
@@ -49,10 +49,12 @@ public class BankService {
         boolean rsl = false;
         Account src = findByRequisite(srcPassport, srcRequisite);
         Account dest = findByRequisite(destPassport, destRequisite);
-        if (src.getBalance() >= amount) {
-            src.setBalance(src.getBalance() - amount);
-            dest.setBalance(dest.getBalance() + amount);
-            rsl = true;
+        if (src != null && dest != null) {
+            if (src.getBalance() >= amount) {
+                src.setBalance(src.getBalance() - amount);
+                dest.setBalance(dest.getBalance() + amount);
+                rsl = true;
+            }
         }
         return rsl;
     }
