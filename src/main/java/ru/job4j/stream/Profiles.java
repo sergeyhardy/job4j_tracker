f70176e8a4315468e1e27ad4a89930b568fdbbfd
sergeyhardy <sergeyhardy@gmail.com>
2022-07-05 23:34:38 +0900

    2. Список адресов [#110062 #318699]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 }
